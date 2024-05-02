51d380773acfbf5c9c170605c83e299bab84fcea
muthuamr <muthu.amr@gmail.com>
2024-05-02 17:11:21 +1200

    Include toast notification

--- src/components/NewOrderPage.tsx
+++ src/components/NewOrderPage.tsx
@@ -2,16 +2,46 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addOrder } from '../redux/actions';
 import '../styles/NewOrder.css';
+import { toast,ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from 'react-router-dom';
+import uuid from "react-uuid";
 
-const NewOrderPage: React.FC = () => {
-  const [id, setId] = useState('');
+  const NewOrderPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const [id, setId] = useState(uuid);
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [description, setDescription] = useState('');
   const [quantity, setQuantity] = useState('');
   const dispatch = useDispatch();
-
+  
   const handleSubmit = () => {
+  let numquantity=parseInt(quantity);
+    
+      if(!lastName)
+      {
+          toast.error('Last Name is required!');
+          
+          return;
+      }
+      if(!description)
+      {
+          toast.error('Description is required!');
+          return;
+      }          
+      if(isNaN(numquantity))
+      {
+          toast.error('Quantity is required!');
+          return;
+      }
+      if(numquantity<1 || numquantity>20)
+      {
+          toast.error('Quantity must be a number between 1 and 20');
+          return;
+      }
+  
     dispatch(addOrder({
       id,
       firstName,
@@ -25,26 +55,20 @@ const NewOrderPage: React.FC = () => {
     setLastName('');
     setDescription('');
     setQuantity('');
+    navigate('/');
+
   };
 
   return (
     <div className="form-container">
       <h2>Add New Order</h2>
       <div className="form-group">
-        <label htmlFor="orderId">Order Id:</label>
-        <input
-          id="orderId"
-          type="text"
-          value={id}
-          onChange={e => setId(e.target.value)}
-        />
-      </div>
-      <div className="form-group">
         <label htmlFor="firstName">First Name:</label>
         <input
           id="firstName"
           type="text"
           value={firstName}
+          maxLength={20}
           onChange={e => setFirstName(e.target.value)}
         />
       </div>
@@ -54,6 +78,7 @@ const NewOrderPage: React.FC = () => {
           id="lastName"
           type="text"
           value={lastName}
+          maxLength={20}
           onChange={e => setLastName(e.target.value)}
         />
       </div>
@@ -63,6 +88,7 @@ const NewOrderPage: React.FC = () => {
           id="orderDescription"
           type="text"
           value={description}
+          maxLength={100}
           onChange={e => setDescription(e.target.value)}
         />
       </div>
@@ -72,6 +98,8 @@ const NewOrderPage: React.FC = () => {
           id="quantity"
           type="number"
           value={quantity}
+          minLength={1}
+          maxLength={20}
           onChange={e => setQuantity(e.target.value)}
         />
       </div>
@@ -82,6 +110,8 @@ const NewOrderPage: React.FC = () => {
       >
         Submit
       </button>
+
+      <ToastContainer position="top-right"></ToastContainer> 
     </div>
   );
 };
